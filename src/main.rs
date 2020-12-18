3f76b769a188b3bcbee9e1ce21d8352c6dadd50a
PockelHockel <pockelhockel1@gmail.com>
2020-12-18 13:18:43 +0100

    Add rocket

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,16 @@
  * Copyright (c) 2020, PockelHockel, All rights reserved.
  */
 
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![index]).launch();
+}
+
+#[get("/")]
+fn index() -> String {
+    String::from("Hello, world!")
 }
