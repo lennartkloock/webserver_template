12a9025502d19a8ea5a6a6aefc0cf807d0b0abf7
Lennart Kloock <lennart.kloock@gmx.de>
2021-04-01 19:54:51 +0200

    Use rocket's included tokio

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ extern crate rocket;
 #[macro_use]
 extern crate serde;
 
+use rocket::tokio;
+
+
 #[tokio::main]
 async fn main() -> Result<(), rocket::error::Error> {
     rocket::ignite().mount("/", routes![index]).launch().await
