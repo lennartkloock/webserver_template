007054c468b12c08f04f49581a1ab4f29f43e14f
PockelHockel <lennart.kloock@gmx.de>
2021-04-01 12:14:42 +0200

    Make async

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,17 @@
  * Copyright (c) 2021, PockelHockel, All rights reserved.
  */
 
-//! TODO: `rustup default nightly`
-
-#![feature(proc_macro_hygiene, decl_macro)]
-
 #[macro_use]
 extern crate rocket;
+#[macro_use]
+extern crate serde;
 
-fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+#[tokio::main]
+async fn main() -> Result<(), rocket::error::Error> {
+    rocket::ignite().mount("/", routes![index]).launch().await
 }
 
 #[get("/")]
-fn index() -> String {
+async fn index() -> String {
     String::from("Hello, world!")
 }
